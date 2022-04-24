2c562402d9522fdb412cfa46ef296baf0f2bdd5e
Devecor <devecor@outlook.com>
2022-04-24 15:46:43 +0800

    Add web app

---
+++ web/src/App.vue
@@ -0,0 +1,21 @@
+
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios'
+const username = ref('')
+const password = ref('')
+function login() {
+	axios.post('/login', {
+		username: username.value,
+		password: password.value
+	}).then((res) => {
+		console.log(res)
+	})
+}
+</script>
+
+<template>
+	<el-input v-model="username" placeholder="username" clearable />
+	<el-input v-model="password" placeholder="password" type="password" show-password clearable />
+	<el-button type="primary" @click="login">Login</el-button>
+</template>
