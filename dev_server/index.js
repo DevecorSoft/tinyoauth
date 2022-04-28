ee6d31443abaa8a564691fef39c0d7095d665c09
Devecor <devecor@outlook.com>
2022-04-28 10:20:08 +0800

    shut down server with shutdown api

--- dev_server/index.js
+++ dev_server/index.js
@@ -3,7 +3,6 @@ const { app } = require("../app");
 console.log("somejjjjjj")
 app.get("/shutdown", (req, res) => {
     console.log("bye...\n")
-    // require("process").exit(0)
-    process.exit(1)
+    require("process").exit(0)
 })
 app.listen(3333);
--- test/apitest/post-login.test.js
+++ test/apitest/post-login.test.js
@@ -1,13 +1,10 @@
-const { exec } = require("child_process");
 const axios = require("axios");
 const expect = require("chai").expect;
 
 describe("Given a correct pair of username and password", () => {
-  let app;
   before(() => {
-    // require("../../server")
-    app = exec("pwd && node -v && node dev_server", );
-    console.log("before ended")
+    require("../../dev_server")
+    console.log("before endded")
   });
 
   describe("When it post /login api", () => {
@@ -25,9 +22,9 @@ describe("Given a correct pair of username and password", () => {
     console.log("after started")
     axios.get('/shutdown').then(data => {
       console.log(data)
+    }).catch((err) => {
+      console.log(err)
     })
-
-    app.kill()
     console.log("after ended")
   });
 });
