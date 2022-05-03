d03cabcdc753efbe4d0e02efd4843905de03eb0c
Devecor <devecor@outlook.com>
2022-05-03 21:40:44 +0800

    more test for login verify

--- app/login/service.js
+++ app/login/service.js
@@ -1,8 +1,17 @@
-function login_service (login_repo) {
-    this.login_repo = login_repo
+function login_service(login_repo) {
+  this.login_repo = login_repo;
 }
 
-login_service.prototype.verify = function(username, password) {}
-login_service.prototype.set_status = function(username, status) {}
+login_service.prototype.verify = function (username, password) {
+  const user = this.login_repo.find_user_by_user_name(username);
+  if (user) {
+    if (user.password === password) {
+      return true;
+    }
+    return false;
+  }
+  return false;
+};
+login_service.prototype.set_status = function (username, status) {};
 
-exports.LoginService = login_service
+exports.LoginService = login_service;
--- test/unittest/login/login-service.test.js
+++ test/unittest/login/login-service.test.js
@@ -7,14 +7,44 @@ describe("Given a valid pair of user name and password", () => {
     it("Then should return true to indicate successful authentication", () => {
       const repo = {
         find_user_by_user_name: sinon.fake.returns({
-            username: "user",
-            password: "pass",
-            status: "offline"
-        })
-      }
+          username: "user",
+          password: "pass",
+          status: "offline",
+        }),
+      };
       const login_service = new LoginService(repo);
-      const res = login_service.verify("user", "pass")
-      expect(res).to.be.true
+      const res = login_service.verify("user", "pass");
+      expect(res).to.be.true;
+    });
+  });
+});
+
+describe("Given a invalid pair of user name and password", () => {
+  describe("When verify password by login service", () => {
+    it("Then should return false to indicate a failed authentication", () => {
+      const repo = {
+        find_user_by_user_name: sinon.fake.returns({
+          username: "user",
+          password: "pass",
+          status: "offline",
+        }),
+      };
+      const login_service = new LoginService(repo);
+      const res = login_service.verify("user", "wrong password");
+      expect(res).to.be.false;
+    });
+  });
+});
+
+describe("Given a non-existent user", () => {
+  describe("When verify password by login service", () => {
+    it("Then shuld also return false to indicate a failed authentication", () => {
+      const repo = {
+        find_user_by_user_name: sinon.fake.returns(null),
+      };
+      const login_service = new LoginService(repo);
+      const res = login_service.verify("non-existent user", "password");
+      expect(res).to.be.false;
     });
   });
 });
