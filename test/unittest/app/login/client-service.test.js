e727cee419f010426f691b60a3b0f5e856f387f8
Devecor <devecor@outlook.com>
2022-05-20 15:49:54 +0800

    add missing test

---
+++ test/unittest/app/login/client-service.test.js
@@ -0,0 +1,40 @@
+const sinon = require("sinon");
+const { expect } = require("chai");
+const { ClientService } = require("../../../../app/login/service");
+
+describe("Given user try to login our system", () => {
+  describe("When user password authenticated", () => {
+    const fake_supplier = {
+      generate_cid: sinon.fake.returns("my client id"),
+      generate_secret: sinon.fake.returns("my client secret"),
+    };
+    const fake_repo = {
+      create_client_identifier: sinon.fake(),
+    };
+    const client_service = new ClientService(fake_supplier, fake_repo);
+    const client_identifier = client_service.issue_identifier("user");
+
+    it("Then should issue client identifier", () => {
+      expect(fake_supplier.generate_cid.calledOnce).to.be.true;
+      expect(fake_supplier.generate_secret.calledOnce).to.be.true;
+      expect(client_identifier).to.be.deep.equal({
+        client_id: "my client id",
+        client_secret: "my client secret",
+      });
+    });
+
+    it("Then should save generated client identifier into db via repository", () => {
+      expect(fake_repo.create_client_identifier.calledOnce).to.be.true;
+      const create_client_identifier_args =
+        fake_repo.create_client_identifier.getCall(0).args;
+
+      expect(create_client_identifier_args).to.be.deep.equal([
+        {
+          user: "user",
+          client_id: "my client id",
+          client_secret: "my client secret",
+        },
+      ]);
+    });
+  });
+});
