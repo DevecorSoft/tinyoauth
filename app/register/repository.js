2ba01d12d7fd776426d56cc8b9da9b1e321fe6c4
Devecor <devecor@outlook.com>
2022-05-22 14:29:27 +0800

    fix silly mistakes

--- app/register/repository.js
+++ app/register/repository.js
@@ -3,27 +3,36 @@ const { DynamoDBClient, PutItemCommand } = require("@aws-sdk/client-dynamodb");
 /**
  * @constructor
  * @param {DynamoDBClient} dynamodb
+ * @param {import("../login/repository").TimeSupplier} time_suppiler - time supplier
  */
-function register_repository(dynamodb) {
+function register_repository(dynamodb, time_suppiler) {
   this.ddbclient = dynamodb;
+  this.time_suppiler = time_suppiler;
 }
-
 
 /**
  * put user info as an item into dynamodb
- * @param {String} username 
- * @param {String} passowrd 
+ * @param {String} username
+ * @param {String} passowrd
  */
-register_repository.prototype.create_user = function (username, passowrd) {
-  this.ddbclient.send(new PutItemCommand({
-    TableName: "tinyoauth_user",
-    Item: {
-      username: { S: username },
-      password: { S: passowrd },
-      user_status: { S: "online" },
-      operation_time: { S: "Wed, 14 Jun 2017 07:00:00 GMT" },
-    },
-  }));
+register_repository.prototype.create_user = function (
+  username,
+  passowrd,
+  user_id
+) {
+  this.ddbclient.send(
+    new PutItemCommand({
+      TableName: "tinyoauth_user",
+      Item: {
+        username: { S: username },
+        user_id: { S: user_id },
+        password: { S: passowrd },
+        user_status: { S: "online" },
+        operation_time: { S: this.time_suppiler.utc_now },
+        creation_time: { S: this.time_suppiler.utc_now },
+      },
+    })
+  );
 };
 
 exports.RegisterRepository = register_repository;
--- test/unittest/app/register/register-repository.test.js
+++ test/unittest/app/register/register-repository.test.js
@@ -5,22 +5,30 @@ const { RegisterRepository } = require("../../../../app/register/repository");
 describe("Given guest's gonna sign up", () => {
   describe("When put user info as an item into dynamodb", () => {
     it("Then call aws-sdk with required user name and password", () => {
-      fake_db = {
+      const fake_db = {
         send: sinon.fake(),
       };
-      const register_repo = new RegisterRepository(fake_db);
-      register_repo.create_user("user", "my pass");
+      const time_suppiler = {};
+      const fake_utc_now = sinon.fake.returns("Wed, 14 Jun 2017 07:00:00 GMT");
+      Object.defineProperty(time_suppiler, "utc_now", {
+        get: fake_utc_now,
+      });
+      const register_repo = new RegisterRepository(fake_db, time_suppiler);
+      register_repo.create_user("user", "my pass", "my user id");
 
       expect(fake_db.send.calledOnce).to.be.true;
+      expect(fake_utc_now.calledTwice).to.be.true;
 
       const args = fake_db.send.getCall(0).args[0].input;
       expect(args).to.be.deep.equal({
         TableName: "tinyoauth_user",
         Item: {
           username: { S: "user" },
+          user_id: { S: "my user id" },
           password: { S: "my pass" },
           user_status: { S: "online" },
           operation_time: { S: "Wed, 14 Jun 2017 07:00:00 GMT" },
+          creation_time: { S: "Wed, 14 Jun 2017 07:00:00 GMT" },
         },
       });
     });
