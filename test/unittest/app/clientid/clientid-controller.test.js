2ada9dba8f143461be9c74adcfe9d80eb7df6605
Devecor <devecor@outlook.com>
2022-06-09 16:10:56 +0800

    test for mandatory field: client_type

--- test/unittest/app/clientid/clientid-controller.test.js
+++ test/unittest/app/clientid/clientid-controller.test.js
@@ -30,5 +30,20 @@ describe("Given to handle a client issuing request", () => {
       expect(fake_res.status.calledOnce).to.be.true;
       expect(fake_res.status.getCall(0).args).to.be.deep.equal([400]);
     });
+
+    it("Then should set http status with 400 in case of missing client_type", () => {
+      const fake_res = { json: Sinon.fake(), status: Sinon.fake() };
+      const fake_req = {
+        body: {
+          name: "client name",
+        },
+        header: Sinon.fake.returns("Bearer xxx"),
+      };
+      const client_id_controller = new ClientIdController();
+      client_id_controller.handler(fake_req, fake_res);
+
+      expect(fake_res.status.calledOnce).to.be.true;
+      expect(fake_res.status.getCall(0).args).to.be.deep.equal([400]);
+    });
   });
 });
