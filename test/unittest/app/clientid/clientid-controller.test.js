aba236104c676506afe86e34913823097522cc4a
Devecor <devecor@outlook.com>
2022-06-09 16:06:19 +0800

    test for mandatory field: name

--- test/unittest/app/clientid/clientid-controller.test.js
+++ test/unittest/app/clientid/clientid-controller.test.js
@@ -12,8 +12,23 @@ describe("Given to handle a client issuing request", () => {
       const client_id_controller = new ClientIdController();
       client_id_controller.handler(fake_req, fake_res);
 
-      expect(fake_res.status.calledOnce);
-      expect(fake_res.status.getCall(0).args).to.be.equal([401]);
+      expect(fake_res.status.calledOnce).to.be.true;
+      expect(fake_res.status.getCall(0).args).to.be.deep.equal([401]);
+    });
+  });
+
+  describe("When user did login", () => {
+    it("Then should set http status with 400 in case of missing name", () => {
+      const fake_res = { json: Sinon.fake(), status: Sinon.fake() };
+      const fake_req = {
+        body: {},
+        header: Sinon.fake.returns("Bearer xxx"),
+      };
+      const client_id_controller = new ClientIdController();
+      client_id_controller.handler(fake_req, fake_res);
+
+      expect(fake_res.status.calledOnce).to.be.true;
+      expect(fake_res.status.getCall(0).args).to.be.deep.equal([400]);
     });
   });
 });
