7215da211142b630ae5c58466676fcd62a76efd7
Devecor <devecor@outlook.com>
2022-06-09 16:00:32 +0800

    impl to pass test for 401

--- app/clientid/controller.js
+++ app/clientid/controller.js
@@ -10,6 +10,15 @@
 function client_id_controller() {}
 
 /**
+ * post client api handler
+ * @param {request} req
+ * @param {response} res
+ */
+client_id_controller.prototype.handler = function (req, res) {
+  res.status(401);
+};
+
+/**
  * @class
  */
- exports.ClientIdController = client_id_controller;
+exports.ClientIdController = client_id_controller;
