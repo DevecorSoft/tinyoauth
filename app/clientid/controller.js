ecac6db2e72abbe9374b9e8fe8bded35ff6ec63a
Devecor <devecor@outlook.com>
2022-06-09 14:40:09 +0800

    add add empty client id controller

---
+++ app/clientid/controller.js
@@ -0,0 +1,15 @@
+/**
+ * clientid controller module
+ * @module app/clientid/controller
+ */
+
+/**
+ * @alias ClientIdController
+ * @constructor
+ */
+function client_id_controller() {}
+
+/**
+ * @class
+ */
+ exports.ClientIdController = client_id_controller;
