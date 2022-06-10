a9f8dafa84dbf714030241288bec1eccf23f6162
Devecor <devecor@outlook.com>
2022-06-10 10:00:07 +0800

    test for client issuing

--- test/unittest/app/clientid/clientid-controller.test.js
+++ test/unittest/app/clientid/clientid-controller.test.js
@@ -23,85 +23,115 @@ describe("Given to handle a client issuing request", () => {
   });
 
   describe("When user did login", () => {
-    it("Then should set http status with 400 in case of missing name", () => {
+    it("Then should issue client", () => {
       const fake_res = {};
-      fake_res.send = Sinon.fake();
-      fake_res.status = Sinon.fake.returns(fake_res);
-
-      const fake_req = {
-        body: {},
-        header: Sinon.fake.returns("Bearer xxx"),
-      };
-      const client_id_controller = new ClientIdController();
-      client_id_controller.handler(fake_req, fake_res);
-
-      expect(fake_res.status.calledOnce).to.be.true;
-      expect(fake_res.status.getCall(0).args).to.be.deep.equal([400]);
-      expect(fake_res.send.calledOnce).to.be.true;
-      expect(fake_res.send.getCall(0).args).to.be.deep.equal([""]);
-    });
-
-    it("Then should set http status with 400 in case of missing client_type", () => {
-      const fake_res = {};
-      fake_res.send = Sinon.fake();
-      fake_res.status = Sinon.fake.returns(fake_res);
-
-      const fake_req = {
-        body: {
-          name: "client name",
-        },
-        header: Sinon.fake.returns("Bearer xxx"),
-      };
-      const client_id_controller = new ClientIdController();
-      client_id_controller.handler(fake_req, fake_res);
-
-      expect(fake_res.status.calledOnce).to.be.true;
-      expect(fake_res.status.getCall(0).args).to.be.deep.equal([400]);
-      expect(fake_res.send.calledOnce).to.be.true;
-      expect(fake_res.send.getCall(0).args).to.be.deep.equal([""]);
-    });
-
-    it("Then should set http status with 400 in case of missing authorization_grant_type", () => {
-      const fake_res = {};
-      fake_res.send = Sinon.fake();
+      fake_res.json = Sinon.fake();
       fake_res.status = Sinon.fake.returns(fake_res);
 
       const fake_req = {
         body: {
           name: "client name",
           client_type: "public",
+          authorization_grant_type: "authorization code",
+          redirect_urls: ["url"],
         },
         header: Sinon.fake.returns("Bearer xxx"),
       };
-      const client_id_controller = new ClientIdController();
-      client_id_controller.handler(fake_req, fake_res);
 
-      expect(fake_res.status.calledOnce).to.be.true;
-      expect(fake_res.status.getCall(0).args).to.be.deep.equal([400]);
-      expect(fake_res.send.calledOnce).to.be.true;
-      expect(fake_res.send.getCall(0).args).to.be.deep.equal([""]);
-    });
-
-    it("Then should set http status with 400 in case of missing redirect_urls", () => {
-      const fake_res = {};
-      fake_res.send = Sinon.fake();
-      fake_res.status = Sinon.fake.returns(fake_res);
-
-      const fake_req = {
-        body: {
-          name: "client name",
-          client_type: "public",
-          authorization_grant_type: "authorization code"
-        },
-        header: Sinon.fake.returns("Bearer xxx"),
+      const fake_client_service = {
+        issue_identifier: Sinon.fake(),
       };
-      const client_id_controller = new ClientIdController();
+
+      const client_id_controller = new ClientIdController(fake_client_service);
       client_id_controller.handler(fake_req, fake_res);
 
-      expect(fake_res.status.calledOnce).to.be.true;
-      expect(fake_res.status.getCall(0).args).to.be.deep.equal([400]);
-      expect(fake_res.send.calledOnce).to.be.true;
-      expect(fake_res.send.getCall(0).args).to.be.deep.equal([""]);
+      expect(fake_client_service.issue_identifier.calledOnce).to.be.true;
+      expect(
+        fake_client_service.issue_identifier.getCall(0).args
+      ).to.be.deep.equal([""]);
     });
+  });
+});
+
+describe("Given the mandatory fields are not met", () => {
+  it("Then should set http status with 400 in case of missing name", () => {
+    const fake_res = {};
+    fake_res.send = Sinon.fake();
+    fake_res.status = Sinon.fake.returns(fake_res);
+
+    const fake_req = {
+      body: {},
+      header: Sinon.fake.returns("Bearer xxx"),
+    };
+    const client_id_controller = new ClientIdController();
+    client_id_controller.handler(fake_req, fake_res);
+
+    expect(fake_res.status.calledOnce).to.be.true;
+    expect(fake_res.status.getCall(0).args).to.be.deep.equal([400]);
+    expect(fake_res.send.calledOnce).to.be.true;
+    expect(fake_res.send.getCall(0).args).to.be.deep.equal([""]);
+  });
+
+  it("Then should set http status with 400 in case of missing client_type", () => {
+    const fake_res = {};
+    fake_res.send = Sinon.fake();
+    fake_res.status = Sinon.fake.returns(fake_res);
+
+    const fake_req = {
+      body: {
+        name: "client name",
+      },
+      header: Sinon.fake.returns("Bearer xxx"),
+    };
+    const client_id_controller = new ClientIdController();
+    client_id_controller.handler(fake_req, fake_res);
+
+    expect(fake_res.status.calledOnce).to.be.true;
+    expect(fake_res.status.getCall(0).args).to.be.deep.equal([400]);
+    expect(fake_res.send.calledOnce).to.be.true;
+    expect(fake_res.send.getCall(0).args).to.be.deep.equal([""]);
+  });
+
+  it("Then should set http status with 400 in case of missing authorization_grant_type", () => {
+    const fake_res = {};
+    fake_res.send = Sinon.fake();
+    fake_res.status = Sinon.fake.returns(fake_res);
+
+    const fake_req = {
+      body: {
+        name: "client name",
+        client_type: "public",
+      },
+      header: Sinon.fake.returns("Bearer xxx"),
+    };
+    const client_id_controller = new ClientIdController();
+    client_id_controller.handler(fake_req, fake_res);
+
+    expect(fake_res.status.calledOnce).to.be.true;
+    expect(fake_res.status.getCall(0).args).to.be.deep.equal([400]);
+    expect(fake_res.send.calledOnce).to.be.true;
+    expect(fake_res.send.getCall(0).args).to.be.deep.equal([""]);
+  });
+
+  it("Then should set http status with 400 in case of missing redirect_urls", () => {
+    const fake_res = {};
+    fake_res.send = Sinon.fake();
+    fake_res.status = Sinon.fake.returns(fake_res);
+
+    const fake_req = {
+      body: {
+        name: "client name",
+        client_type: "public",
+        authorization_grant_type: "authorization code",
+      },
+      header: Sinon.fake.returns("Bearer xxx"),
+    };
+    const client_id_controller = new ClientIdController();
+    client_id_controller.handler(fake_req, fake_res);
+
+    expect(fake_res.status.calledOnce).to.be.true;
+    expect(fake_res.status.getCall(0).args).to.be.deep.equal([400]);
+    expect(fake_res.send.calledOnce).to.be.true;
+    expect(fake_res.send.getCall(0).args).to.be.deep.equal([""]);
   });
 });
