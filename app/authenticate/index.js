4e1304221c7931d7a6b264bb8ffda253c81418bb
Devecor <devecor@outlook.com>
2022-06-13 14:53:14 +0800

    configure entry of authenticate handler

--- app/authenticate/index.js
+++ app/authenticate/index.js
@@ -3,12 +3,19 @@
  * @module app/authenticate
  */
 
+const { JwtSupplier } = require("../../lib/jwtSupplier");
+const { AuthenticateController } = require("./controller");
+
+const authenticate_controller = new AuthenticateController(JwtSupplier);
+
 /**
  * authenticate user handler
  * @function
  * @param {request} req - express request
  * @param {response} res - express response
  * @param {Function} next - express next
+ * @see AuthenticateController#handler
  */
-exports.authenticate = (req, res, next) => {
-};
+exports.authenticate = authenticate_controller.handler.bind(
+  authenticate_controller
+);
